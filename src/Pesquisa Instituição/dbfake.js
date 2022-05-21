e436bde827edc1d07eaf2a99cc4b693e55ac452d
Karen Noguti <jkjvida@hotmail.com>
2022-05-21 20:24:10 +0900

    Pesquisa de Instituição

---
+++ src/Pesquisa Instituição/dbfake.js
@@ -0,0 +1,36 @@
+var db = {
+    dados: [
+        {
+            Instituição: 'Hospital Nossa Senhora',
+            CNPJ: '90.953.444/0001-00',
+            Telefone:'(31)3612-1245',
+            Cidade:'Belo Horizonte',
+            Estado:'Minas Gerais',
+
+        },{
+            Instituição: 'Hospital Vila Velha',
+            CNPJ: '90.953.555/0001-00',
+            Telefone:'(11)4692-1245',
+            Cidade:'Biritiba Mirim',
+            Estado:'São Paulo',
+        },{
+            Instituição: 'Clinica São Pedro',
+            CNPJ: '90.953.666/0001-00',
+            Telefone:'(21)2222-1245',
+            Cidade:'Niteroi',
+            Estado:'Rio de Janeiro',
+        },{
+            Instituição: 'Hospital Referência',
+            CNPJ: '90.953,777/0001-00',
+            Telefone:'(1)4444-1245',
+            Cidade:'Curitiba',
+            Estado:'Paraná',
+        },{
+            Instituição: 'Centro de Reabilitação Vale do Paraíba',
+            CNPJ: '90.953.888/0001-00',
+            Telefone:'(41)5555-1245',
+            Cidade:'Belo Horizonte',
+            Estado:'Espirito Santo',
+        }
+    ]
+}
