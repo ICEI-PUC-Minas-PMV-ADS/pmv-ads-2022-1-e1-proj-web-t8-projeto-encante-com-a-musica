10ed28faaf8328af8c59f1eb76cd235945ccbe2c
keirosu <103314640+keirosu@users.noreply.github.com>
2022-05-29 19:58:55 +0900

    atualização pagina inicial

---
+++ src/Página Inicial/dbfake.js
@@ -0,0 +1,25 @@
+var db = {
+    dados:[
+      {
+        titulo: 'Depoimentos',
+        nome:'Luana Oliveira', 
+        img:"https:source.unsplash.com/random/450x200?sig=0",  
+        idade:'32 anos',
+        texto: '"Quer que todos que recebam o diagnóstico de uma doença incurável como a sua, possam ter o tratamento paliativo."'
+      }, {
+        titulo: 'Depoimentos',
+        nome:'Amanda Ribeiro',
+        img:"https:source.unsplash.com/random/450x200?sig=1",
+        idade:'53 anos',
+        texto: '"Quer levar a música como fonte para aliviar as dores e sofrimentos das pessoas."'
+      }, {
+        titulo: 'Depoimentos',
+        nome:'Adriano Oliveira',
+        img:"https:source.unsplash.com/random/450x200?sig=2",
+        idade:'35 anos',
+        texto: '"Nunca realizou trabalho voluntário, mas ao pesquisar grupos para treinar violão, descobriu sobre trabalho voluntário com música."'
+      
+     
+      }  
+    ]
+}
