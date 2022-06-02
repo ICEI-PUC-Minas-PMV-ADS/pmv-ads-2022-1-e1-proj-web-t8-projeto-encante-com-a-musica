f60f8bcb5e16f9859650c06d4f70f9fedc005f1e
Carolina-Laurindo <103545958+Carolina-Laurindo@users.noreply.github.com>
2022-06-02 20:05:42 -0300

    Criação pesquisa de voluntários

---
+++ src/Pesquisa Vountarios/db_fake.js
@@ -0,0 +1,150 @@
+var db = {
+  grupos: [
+    {
+      nome: "Vozes do amor ",
+      cidade: "Biritiba Mirim",
+      estado: "São Paulo",
+      instituicao: "Hospital Ribas",
+      email:"v_amor@gmail.com"
+  },{
+    nome: "Doutores da Musica",
+    cidade: "Curitiba",
+    estado: "Paraná",
+    instituicao: "Hospital Referência",
+    email:"drs_musica@yahoo.com"
+  },{
+    nome: "Cantando com alegria",
+    cidade: "Manaus",
+    estado: "Amazonas",
+    instituicao: "Hospital Ribas",
+    email:"alegria@gmail.com"
+  },{
+    nome: "Musica com amor ",
+    cidade: "Niteroi",
+    estado: "Rio de Janeiro",
+    instituicao: "Clínica São Pedro",
+    email:"m_amor@gmail.com"
+  }],
+
+voluntarios:[
+  {
+    "nome":"Arthur Gustavo Barbosa",
+      "cidade":"Sorocaba",
+      "estado":"São Paulo",
+      "celular":"(11)99354-8968",
+      "email":"Arthur.@ciobcv.com.br",
+      "cat_voz": "soprano",
+      "cat_instrumento":["flauta doce","violao"],
+    }, {
+      "nome": "Raul Sebastião Levi Oliveira",
+      "email": "raulsebastiaooliveira@lctour.com.br",
+      "cidade": "Imperatriz",
+      "estado": "MA",
+      "celular": "(98) 99375-9541",
+      "cat_voz": "soprano",
+      "cat_instrumento":"gaita",
+      
+    },
+    {
+      "nome": "Aurora Ester Moraes",
+      "email": "aurora_moraes@consorciobcv.com.br",
+      "cidade": "Cacoal",
+      "estado": "RO",
+      "celular": "(69) 99407-9451",
+      "cat_voz": "baritono",
+      "cat_instrumento":["flauta doce","gaita"],
+      
+    },
+    {
+      "nome": "Adriana Vanessa Isadora Baptista",
+      "email": "adriana_vanessa_baptista@flir.com.br",
+      "cidade": "Colombo",
+      "estado": "PR",
+      "celular": "(41) 99539-3927",
+      "altura": "1,63",
+      "cat_voz": "tenor",
+      "cat_instrumento":["gaita,violão"],
+    },
+    {
+      "nome": "Clara Stefany Valentina Jesus",
+      "email": "clara_jesus@lucaslima.com",
+      "cidade": "Teresina",
+      "estado": "PI",
+      "celular": "(86) 98915-0175",
+      "cat_voz": "baritono",
+      "cat_instrumento":["gaita","violao"],
+    },
+    {
+      "nome": "Luzia Flávia Cardoso",
+      "email": "luzia_flavia_cardoso@bsd.com.br",
+      "cidade": "Feira de Santana",
+      "estado": "BA",
+      "celular": "(75) 99600-2179",
+      "cat_voz": "soprano",
+      "cat_instrumento":"violão",
+    },
+    {
+      "nome": "Vera Rayssa Rebeca Assis",
+      "email": "verarayssaassis@rafaelsouza.com.br",
+      "cidade": "Aparecida de Goiânia",
+      "estado": "GO",
+      "celular": "(62) 98926-5108",
+      "cat_voz": "tenor",
+      "cat_instrumento":"violão",
+    },
+    {
+      "nome": "Geraldo Manoel de Paula",
+      "email": "geraldo.manoel.depaula@landovale.com.br",
+      "senha": "NjKhJjJ6Jb",
+      "cidade": "São Luís",
+      "estado": "MA",
+      "celular": "(98) 99886-7704",
+      "cat_voz": "baritono",
+      "cat_instrumento":"flauta doce",
+      
+    },
+    {
+      "nome": "Lucca Sebastião Sales",
+      "email": "lucca-sales99@grupointegraambiental.com.br",
+      "cidade": "Rio Branco",
+      "estado": "AC",
+      "celular": "(68) 98526-1192",
+      "cat_voz": "soprano",
+      "cat_instrumento":"gaita",
+    },
+    {
+      "nome": "Fátima Helena Isabelly Campos",
+      "email": "fatima_helena_campos@coraza.com.br",
+      "cidade": "Manaus",
+      "estado": "AM",
+      "celular": "(92) 99128-0423",
+      "cat_voz": "tenor",
+      "cat_instrumento":"flauta doce",
+    },
+    {
+      "nome": "Daiane Alícia Alves",
+      "email": "daiane-alves88@vivax.com",
+      "cidade": "Joinville",
+      "estado": "SC",
+      "celular": "(47) 98759-2809",
+      "cat_voz": "tenor",
+      "cat_instrumento":"flauta doce",
+    },
+    {
+      "nome": "Sophia Mariana Aparício",
+      "email": "sophia_mariana_aparicio@alliancarh.com.br",
+      "cidade": "Santa Maria",
+      "estado": "RS",
+      "celular": "(55) 99838-0590",
+      "cat_voz": "soprano",
+      "cat_instrumento":"violão",
+    },
+    
+]
+  }
+
+
+
+
+
+
