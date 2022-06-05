bd54af4daec9e94484d2ebf2a33c55ac4b258665
ClaudiaCarapiaFerraz <102997121+ClaudiaCarapiaFerraz@users.noreply.github.com>
2022-06-05 17:56:33 -0300

    atualização

---
+++ src/Sobre/db_fake.js
@@ -0,0 +1,20 @@
+var db = {
+    artigos: [
+        {
+            titulo: "O uso da música nos cuidados paliativos: humanizando o cuidado e facilitando o adeus.",
+            autor: "Natalie Hidemi Seki, Sandra Maria Galheigo",
+            link: "https://www.scielo.br/j/icse/a/gYvwrWTLcgJPMF8bpWKN6Jy/?lang=pt"
+           
+        }, {
+            titulo: "A música no alívio da dor em pacientes oncológicos. ",
+            autor: "Andrea Bezerra Rodrigues",
+            link:"https://www.researchgate.net/publication/242199399_A_musica_no_alivio_da_dor_em_pacientes_oncologicos_Music_therapy_in_relief_of_pain_in_oncology_patients"
+            
+        },{
+            titulo: "Melhoria do acesso aos cuidados paliativos.",
+            autor: "OMS",
+            link: "https://www.paho.org/hq/dmdocuments/2016/Palliative-care-infografic-in-Portuguese-WEB.pdf"
+            
+        }
+    ]
+}
