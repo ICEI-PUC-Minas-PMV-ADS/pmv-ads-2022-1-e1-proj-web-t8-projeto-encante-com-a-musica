d3d30e6e5633497c7b7db091fd4b697fd9215699
Karen Noguti <jkjvida@hotmail.com>
2022-06-16 09:03:07 +0900

    Update dbfake.js

--- src/Pagina_inicial/dbfake.js
+++ src/Pagina_inicial/dbfake.js
@@ -3,19 +3,19 @@ var db = {
       {
         titulo: 'Depoimentos',
         nome:'Luana Oliveira', 
-        img:"https://source.unsplash.com/random/450x200?womanface=0",  
+        img:"https://source.unsplash.com/random/450x200?woman=1",  
         idade:'32 anos',
         texto: '"Quer que todos que recebam o diagnóstico de uma doença incurável como a sua, possam ter o tratamento paliativo."'
       }, {
         titulo: 'Depoimentos',
         nome:'Amanda Ribeiro',
-        img:"https://source.unsplash.com/random/450x200?womanface=1",
+        img:"https://source.unsplash.com/random/450x200?womanface=2",
         idade:'53 anos',
         texto: '"Quer levar a música como fonte para aliviar as dores e sofrimentos das pessoas."'
       }, {
         titulo: 'Depoimentos',
         nome:'Adriano Oliveira',
-        img:"https://source.unsplash.com/random/450x200?menface=2",
+        img:"https://source.unsplash.com/random/450x200?menface=3",
         idade:'35 anos',
         texto: '"Nunca realizou trabalho voluntário, mas ao pesquisar grupos para treinar violão, descobriu sobre trabalho voluntário com música."'
       
