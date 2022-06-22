33de61f114bf162425171bbd96170fcaac91eeae
Karen Noguti <jkjvida@hotmail.com>
2022-06-22 14:57:14 +0900

    Update doacao.js

--- src/Página Doação/doacao.js
+++ src/Página Doação/doacao.js
@@ -2,27 +2,27 @@ var dep = {
    date: [
       {
          name:'Leticia Ferreira', 
-         image:"https:source.unsplash.com/random/450x200?woman face=0",  
+         image:"https://source.unsplash.com/random/450x200?woman face=0",  
          text: '"Contribuiu com o projeto pois gostaria que o falecido filho pudesse ter tido acesso ao tratamento quando estava em estágio terminal."'
       }, 
       {
          name:'Gustavo Oliveira',
-         image:"https:source.unsplash.com/random/450x200?men face=0",
+         image:"https://source.unsplash.com/random/450x200?men face=0",
          text: '"Amigo de um voluntário que se sensibilizou com o objetivo do projeto."'
       }, 
       {
          name:'Renata Kimura',
-         image:"https:source.unsplash.com/random/450x200?woman face=1",
+         image:"https://source.unsplash.com/random/450x200?woman face=1",
          text: '"Médica contribuindo para que os pacientes possam ter acesso ao tratamento paliativo."'
       },
       {
          name:'Júlio Damasceno',
-         image:"https:source.unsplash.com/random/450x200?men face=1",
+         image:"https://source.unsplash.com/random/450x200?men face=1",
          text: '"A falecida avó gostava muito de música e o tratamento paliativo teria ajudado ela nos últimos dias."'
       },
       {
          name:'Paulo Andrade',
-         image:"https:source.unsplash.com/random/450x200?men face=2",
+         image:"https://source.unsplash.com/random/450x200?men face=2",
          text: '"É músico mas não consegue se volutariar pela falta de tempo, porém queria ajudar o projeto de alguma forma."'
       }
    ]
